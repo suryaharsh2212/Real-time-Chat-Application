e761f0dc7626e21ed5ee097a23373bb98e379cb3
surya-prakash603 <suryaraj04266@gmail.com>
2024-06-04 22:08:42 +0530

    ..

--- Client/src/Components/Hero.jsx
+++ Client/src/Components/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <div className=" md:h-full h-screen md:-mt-20 overflow-hidden" style={{ backgroundImage: "linear-gradient(to bottom, #FF4500,yellow)" }}>
+        <div className=" md:h-full h-screen md:-mt-20 overflow-hidden" style={{ backgroundImage: "black" }}>
 
 
             <header className="absolute inset-x-0 top-0 z-50" >
