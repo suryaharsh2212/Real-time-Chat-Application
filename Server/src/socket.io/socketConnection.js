7b1fddf84dda37b228eebaf8548307370da8f411
surya-prakash603 <suryaraj04266@gmail.com>
2024-06-14 21:09:42 +0530

    ..

--- Server/src/socket.io/socketConnection.js
+++ Server/src/socket.io/socketConnection.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
   cors:{
-    origin:["https://real-time-chat-application-uyhn.vercel.app"],
+    origin:["https://real-time-chat-application-uyhn.vercel.app/"],
     methods:["GET","POST"]
   }
 });
