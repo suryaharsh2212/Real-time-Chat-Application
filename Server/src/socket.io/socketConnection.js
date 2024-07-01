307c1a2ed135f16abb1634dbe3dc719fadd0990d
surya-prakash603 <suryaraj04266@gmail.com>
2024-07-01 09:37:21 +0530

    socketchanges

--- Server/src/socket.io/socketConnection.js
+++ Server/src/socket.io/socketConnection.js
@@ -2,16 +2,16 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
-import https from "https"
+
 
 const app=express()
 const server = https.createServer(app);
 
 const io = new Server(server,{
-  path:"/socket",
-  wsEngine:['ws','wss'],
-  allowEIO3:true,
-  transports:['websocket','polling'],
+  // path:"/socket",
+  // wsEngine:['ws','wss'],
+  // allowEIO3:true,
+  // transports:['websocket','polling'],
   cors:{
     origin:"*",
     methods:["GET","POST"],
