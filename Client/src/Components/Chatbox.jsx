238051975630b2ec2f2bb6dd9a8ed7e559609a40
Surya prakash <suryaharsh279@gmail.com>
2024-08-06 22:24:04 +0530

    ...

--- Client/src/Components/Chatbox.jsx
+++ Client/src/Components/Chatbox.jsx
@@ -8,8 +8,11 @@ import UseLogoutUser from '../Utility/Uselogout';
 import { UserandomImages } from '../Utility/Usegetimages';
 import UseSearchUser from '../Utility/UserSearchuser';
 import { ToastContainer, toast, Bounce } from 'react-toastify';
-import Ably from 'ably';
+import Ably from 'ably'
 import Friendcard from './Friendcard';
+
+
+
 
 function Chatbox() {
   const { id, name } = useParams();
@@ -19,56 +22,58 @@ function Chatbox() {
   const [datamessage, setdatamessage] = useState([]);
   const [checkselected, setCheckselected] = useState(true);
   const messagesEndRef = useRef(null);
-  const [loading, setloading] = useState(false);
-  const [sentLoading, setsentLoading] = useState(false);
-  const [chattingwith, setchattingwith] = useState("Choose a user to begin chatting");
-  const ably = new Ably.Realtime('OIeztA.vtaYyw:uGJV4_D5wkf4pihnv8M6TcWiyrjaeQSmL1OrCODpIsc');
+  const [loading, setloading] = useState(false)
+  const [sentLoading, setsentLoading] = useState(false)
+  const [chattingwith, setchattingwith] = useState("Choose a user to begin chatting")
+  const ably = new Ably.Realtime('OIeztA.vtaYyw:uGJV4_D5wkf4pihnv8M6TcWiyrjaeQSmL1OrCODpIsc')
 
-  const [Isnewmsg, Setnewmsg] = useState(false);
-  const [search, setSearch] = useState('');
-  const [searchLoading, setsearchLoading] = useState(false);
-  const [userLoading, setuserLoading] = useState(false);
-  const navigate = useNavigate();
-  const [new_msgFrom, setNewmsg] = useState('');
+  const [Isnewmsg, Setnewmsg] = useState(false)
+  const [search, setSearch] = useState('')
+  const [searchLoading, setsearchLoading] = useState(false)
+  const [userLoading, setuserLoading] = useState(false)
+  const navigate = useNavigate()
+  const [new_msgFrom, setNewmsg] = useState('')
+  
 
   const handleMessageChange = async () => {
-    setsentLoading(true);
+    setsentLoading(true)
     setMessage(message);
     await UseSendMessage(message, id, receiverId);
     setdatamessage(prevMessages => [...prevMessages, { message, user: id }]);
     const conversation = await UsegetConversation(id, receiverId._id);
     setdatamessage(conversation.data.messages || []);
-    setsentLoading(false);
+    setsentLoading(false)
     setMessage('');
-  };
 
+
+  };
   useEffect(() => {
-    const channel = ably.channels.get(`${id}`);
+    const channel = ably.channels.get(`${id}`)
     channel.subscribe("new-message", (msg) => {
       Setnewmsg(true);
-      const rec = msg.data.text;
+      const rec = msg.data.text
       setdatamessage(prevMessages => [...prevMessages, { message: rec, user: msg.data.senderId }]);
-    });
-
+    })
     const newMSg = async () => {
       const result = UseCurrentUser(receiverId);
       console.log("Message received from -", result);
-      setNewmsg(result);
-    };
-    newMSg();
+      setNewmsg(result)
+    }
+    newMSg()
 
     return () => {
-      channel.unsubscribe();
+      channel.unsubscribe()
+
     };
   }, []);
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        setuserLoading(true);
+        setuserLoading(true)
         const allUsers = await UseGetUsers(id, receiverId);
         setUsers(allUsers.data);
-        setuserLoading(false);
+        setuserLoading(false)
       } catch (error) {
         console.error('Error fetching users:', error);
       }
@@ -78,10 +83,10 @@ function Chatbox() {
 
   useEffect(() => {
     const fetchdata = async () => {
-      setloading(true);
+      setloading(true)
       const conversation = await UsegetConversation(id, receiverId._id);
       setdatamessage(conversation.data.messages || []);
-      setloading(false);
+      setloading(false)
     };
     fetchdata();
   }, [receiverId._id, id]);
@@ -91,11 +96,9 @@ function Chatbox() {
     setCheckselected(false);
     Setnewmsg(false);
   };
-
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
-
   useEffect(() => {
     scrollToBottom();
   }, [datamessage]);
@@ -109,19 +112,20 @@ function Chatbox() {
       fetchReceiverName();
     }
   }, [receiverId]);
-
   const logout = () => {
-    const log = UseLogoutUser();
+    const log = UseLogoutUser()
     if (log) {
-      navigate("/login");
-    } else {
-      alert("error");
+
+      navigate("/login")
     }
-  };
+    else {
+      alert("error")
+    }
+  }
 
   const searchUser = async (name) => {
-    setsearchLoading(true);
-    const searchResult = await UseSearchUser(name);
+    setsearchLoading(true)
+    const searchResult = await UseSearchUser(name)
 
     if (searchResult.status) {
       toast.success(`${searchResult.message}`, {
@@ -136,8 +140,9 @@ function Chatbox() {
         transition: Bounce,
       });
       setUsers(prevUsers => [searchResult.data, ...prevUsers]);
-      setsearchLoading(false);
-    } else {
+      setsearchLoading(false)
+    }
+    else {
       toast.error(` ${searchResult.message}`, {
         position: "bottom-center",
         autoClose: 5000,
@@ -149,12 +154,14 @@ function Chatbox() {
         theme: "light",
         transition: Bounce,
       });
-      setsearchLoading(false);
+      setsearchLoading(false)
     }
-  };
+  }
+  
+  
 
   return (
-    <div className='bg-whitesmoke'>
+    <div className=' bg-whitesmoke'>
       <ToastContainer
         position="bottom-center"
         autoClose={5000}
@@ -169,6 +176,7 @@ function Chatbox() {
         transition={Bounce}
       />
       <div className='md:grid md:grid-cols-7 p-5 h-screen '>
+
         <div className='md:col-span-2 p-4 overflow-y-scroll h-[100%]  relative -top-4' style={{ scrollbarWidth: '5px', scrollbarColor: 'whitesmoke transparent', borderRadius: '15px' }} >
           <h1 className='w-full btn top-0 mb-5 flex justify-center  text-white' style={{ backgroundImage: "linear-gradient(#0018A8,#1C39BB)" }}>Welcome, {name}</h1>
           <div className="pt-2 relative mx-auto  text-gray-600  mb-5 p-2" >
@@ -193,7 +201,7 @@ function Chatbox() {
               <div className="skeleton h-6 w-full"></div>
             </div>
             :
-            <>
+            <   >
               {Users.map((element, index) => (
                 <button key={index} onClick={() => passdata(element)} className="w-full  btn text-white mb-5  h-fit grid grid-cols-1 sm:grid-cols-5 gap-4 p-3 hover:bg-zinc-300" style={{ backgroundImage: "linear-gradient(transparent,transparent)" }}>
                   <div className="avatar online ml-6 ">
@@ -216,61 +224,110 @@ function Chatbox() {
               ))}
             </>
           }
-          <button className="btn absolute bottom-0 ml-[4%] bg-[#0D5BE1]  hover:bg-[#0018A8] w-[90%] h-[10%]" onClick={logout}>Logout</button>
         </div>
-        <div className='md:col-span-5 bg-whitesmoke'>
-          <div className='border-black border-opacity-20 border-b-2 p-2 text-lg font-serif  text-black-600 flex justify-center' style={{ color: "#1C39BB" }}><p>Chatting with {chattingwith.fullname} </p> </div>
-          {loading
-            ?
-            <div className='flex flex-col  justify-center items-center gap-4 p-4'>
-              <span className="loading loading-ring loading-lg"></span>
-              <h1 className='text-sm font-semibold'>Loading Chat...</h1>
+
+        <div className='col-span-5  md:h-[95%] flex flex-col' >
+
+          <div className='flex flex-row  justify-start items-start'>
+            <h1 className=' ml-5 btn p-5' style={{ width: "90%", backgroundImage: "linear-gradient(#0018A8,#1C39BB)" }}>
+              {loading ?
+                <div className='flex'>
+                  <h1 className=' font-thin text-white'>Loading chats......</h1>
+                  <div className='flex justify-center items-center mb-10'>
+                    <span className="loading loading-spinner loading-md"></span>
+                  </div>
+                </div>
+                :
+                <div className='text-white'>
+                  To : {chattingwith}
+                </div>
+              }
+            </h1>
+            <div data-tip={"Logout"} className=' tooltip tooltip-bottom '>
+              <button onClick={logout} data-tip="Logout" className='border solid  rounded-md hover:bg-slate-200 p-2 ml-3 flex justify-center tooltip-bottom '>
+                <img className='h-7 w-7 tooltip-bottom' data-tip="Logout" src="https://cdn-icons-png.flaticon.com/128/16967/16967536.png" alt="" />
+              </button>
             </div>
-            :
-            <div className='h-[75vh] overflow-y-scroll p-10'>
-              <div className="chat-body">
-                {datamessage.length > 0 ? (
-                  datamessage.map((messageObj, index) => (
-                    <div
-                      key={index}
-                      className={`chat ${messageObj.user === id ? 'chat-end' : 'chat-start'}`}
-                    >
-                      <div
-                        className={`chat-bubble ${messageObj.user === id ? 'bg-blue-600 text-white' : 'bg-gray-300 text-black'}`}
-                      >
-                        {messageObj.message}
-                      </div>
+          </div>
+
+          <div className='flex flex-col flex-grow p-3 bg-white-500'>
+            <div className=' w-full p-3 md:h-[550px] overflow-y-scroll'>
+              {checkselected
+                ?
+                <div className='flex justify-center items-center mt-36 '>
+                  <div className='flex flex-col'>
+                    <h1 className='btn font-light mb-5'> Select a conversation to start........</h1>
+
+                    <div className='flex justify-center ' >
                     </div>
-                  ))
-                ) : (
-                  <p className='text-center text-gray-500'>No messages yet. Start the conversation!</p>
-                )}
-                <div ref={messagesEndRef} />
-              </div>
+                  </div>
+                </div>
+                :
+                <div className=' '>
+                  {datamessage.map((value, index) => {
+                    const task = (value.user == id) ?'end':'start';
+                    const colour = !(value.user == id) ? 'bg-gray-100' : 'bg-gray-100';
+                    return (
+                      <div key={index}  >
+                        <div className={`chat chat-${task}`}>
+                          <div className={`chat-bubble ${colour} text-black`}> {value.message}</div>
+                        </div>
+                      </div>
+
+                    )
+                  })}
+                  <div ref={messagesEndRef} />
+                </div>
+              }
             </div>
-          }
-          <div className="p-4">
-            <div className="input-group">
+          </div>
+          {/* Input message box */}
+          <div className='py-2 px-4 '>
+            <div className='flex w-full mb-5'>
               <input
                 type="text"
-                placeholder="Type a message"
                 value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                className="input input-bordered w-full"
+                onChange={(e) => { setMessage(e.target.value) }}
+                placeholder="Type your message..."
+                className="flex-1 border border-gray-300 w-3/4 rounded-md py-2 px-4 focus:outline-none focus:border-blue-500"
               />
-              <button className="btn btn-primary" onClick={handleMessageChange} disabled={!message || sentLoading}>
-                {sentLoading ? (
-                  <span className="loading loading-spinner"></span>
-                ) : (
-                  'Send'
-                )}
+              <button onClick={handleMessageChange} className="ml-2 bg-blue-700 hover:bg-cyan-600 text-white font-semibold py-2 px-4 rounded-md">
+                {sentLoading
+                  ?
+                  <div>
+                    <span className="loading loading-spinner loading-md"></span>
+                  </div>
+                  :
+                  <div className='text-md'>Send</div>
+                }
               </button>
             </div>
           </div>
         </div>
+        {/* <div className=' ml-2 flex justify-start overflow-y-scroll flex-col'>
+          <div className=' flex w-full justify-center'>
+            <h1 className=' text-lg mt-5 border rounded-lg border-blue-600 font-thin py-2 px-5'>
+            
+            </h1>
+          </div>
+
+          <div className="grid grid-rows-2 gap-3 p-4 sm:grid-cols-0 sm:grid-rows-none">
+
+           
+
+
+            <div className="bg-gray-200  p-4 border text-center">Item 2</div>
+            <div className="bg-gray-200  p-4 border text-center">Item 3</div>
+            <div className="bg-gray-200  p-4 border text-center">Item 4</div>
+          </div>
+
+
+        </div> */}
       </div>
+
     </div>
   );
 }
 
 export default Chatbox;
+
